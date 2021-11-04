3a2c871f895b4ca1e15a27f5d7ff11c73809892a
Shamayana <shamayana777@gmail.com>
2021-11-04 06:08:34 +0300

    feat: solve 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout } = process;
+const file = path.join(__dirname, 'userText.txt');
+const output = fs.createWriteStream(file);
+
+stdout.write('Write some text\n');
+
+stdin.on('data', (data) => {
+  let dataString = data.toString().trim();
+
+  if (dataString == 'exit') {
+    exitProcess();
+  } else {
+    output.write(data);
+  }
+});
+
+function exitProcess() {
+  stdout.write('Good bye! Good luck!');
+  process.exit();
+}
+
+process.on('SIGINT', exitProcess);
