2e2432a1f7d76710ad53c4544e24ea277d528a0f
Shamayana <shamayana777@gmail.com>
2021-11-04 11:53:34 +0300

    feat: solve 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+async function mergeStyles() {
+  const stylesFolder = path.join(__dirname, 'styles');
+  const distFolder = path.join(__dirname, 'project-dist');
+
+  try {
+    const files = await fs.readdir(stylesFolder, { withFileTypes: true });
+    const dataArr = [];
+
+    for (const file of files) {
+      const fileExt = file.name.split('.').slice(-1).toString();
+
+      if (file.isFile() && fileExt == 'css') {
+        const filePath = path.join(stylesFolder, file.name);
+        const data = await fs.readFile(filePath, 'utf-8');
+        dataArr.push(data);
+      }
+    }
+    await fs.writeFile(path.join(distFolder, 'bundle.css'), dataArr.join('\n'));
+    console.log('All styles are in the bundle');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+mergeStyles();
